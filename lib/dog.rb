b5527c5c1c18c6af4eeb12f3885366bd05f91b2c
edwin <ngisahedwin@gmail.com>
2022-10-24 23:12:14 +0300

    commit tree 2aee00c8c4d389b40c48c6fc755290b3db249097

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    def name= (name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed= (breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
+corgi = Dog.new
+puts "name:#{corgi.name = "bosco"}"
+puts corgi.breed = "mastiff"
